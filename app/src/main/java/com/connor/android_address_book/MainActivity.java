5620ee590e006fb83a7e575fa18f6d7a5d982c06
Connor Jenkins <cjconnorjenkins@gmail.com>
2016-12-20 02:00:11 -0500

    messing around, learning Android fundamentals

--- app/src/main/java/com/connor/android_address_book/MainActivity.java
+++ app/src/main/java/com/connor/android_address_book/MainActivity.java
@@ -2,6 +2,10 @@ package com.connor.android_address_book;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView mainListView; // ListView object
+        ArrayAdapter mArrayAdapter; // ArrayAdapter object
+        ArrayList mNameList = new ArrayList();
+
+        // Access the ListView
+        mainListView = (ListView) findViewById(R.id.list);
+
+        // Create an ArrayAdapter for the ListView
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+
+        // Set the ListView to use the ArrayAdapter
+        mainListView.setAdapter(mArrayAdapter);
+
+        mNameList.add("list item");
+        mArrayAdapter.notifyDataSetChanged();
+
+    }
+
+    protected void onPause(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
