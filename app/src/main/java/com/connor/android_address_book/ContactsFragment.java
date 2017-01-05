358693b944b48534ec5950393bc83052038fc3e4
Connor Jenkins <cjconnorjenkins@gmail.com>
2017-01-05 03:27:12 -0500

    changes list entry fab and other tweaks

--- app/src/main/java/com/connor/android_address_book/ContactsFragment.java
+++ app/src/main/java/com/connor/android_address_book/ContactsFragment.java
@@ -39,6 +39,7 @@ public class ContactsFragment extends ListFragment {
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         Intent intent = new Intent(getActivity(), ScrollingAddressEntryActivity.class);
+        intent.putExtra("ID", id);
         startActivity(intent);
     }
 
--- app/src/main/java/com/connor/android_address_book/ScrollingAddressEntryActivity.java
+++ app/src/main/java/com/connor/android_address_book/ScrollingAddressEntryActivity.java
@@ -1,6 +1,11 @@
 package com.connor.android_address_book;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +18,19 @@ public class ScrollingAddressEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling_address_entry);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("ID");
+
+        AddressDatabaseHelper dbHelper = new AddressDatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        Cursor c = db.rawQuery("SELECT * FROM " + AddressDatabaseContract.AddressTable.TABLE_NAME + " WHERE _id=" + id, null);
+        c.moveToFirst();
+        int indexName = c.getColumnIndex(AddressDatabaseContract.AddressTable.NAME);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(c.getString(indexName));
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -24,6 +41,10 @@ public class ScrollingAddressEntryActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Drawable d = getResources().getDrawable(android.R.drawable.ic_menu_edit); // change icon of fab
+        fab.setImageDrawable(d);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
