1e74bd8d37993ecb771b592d101dd19b9e7a1caf
Connor Jenkins <cjconnorjenkins@gmail.com>
2017-01-04 23:24:51 -0500

    list entries are now selectable and start new activity, problems arise when running app on my phone

--- app/src/main/java/com/connor/android_address_book/ContactsFragment.java
+++ app/src/main/java/com/connor/android_address_book/ContactsFragment.java
@@ -1,24 +1,24 @@
 package com.connor.android_address_book;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract.Contacts;
 import android.support.v4.app.ListFragment;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
 public class ContactsFragment extends ListFragment {
 
     private CursorAdapter mAdapter;
-    private static final int layout = android.R.layout.simple_list_item_1;;
+    private static final int layout = android.R.layout.simple_selectable_list_item;
+    private static final String[] FROM = { AddressDatabaseContract.AddressTable.NAME };
+    private static final int[] TO = { android.R.id.text1 };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,29 +26,26 @@ public class ContactsFragment extends ListFragment {
 
         Context context = getActivity();
         AddressDatabaseHelper dbHelper = new AddressDatabaseHelper(context);
-        // get the database, if it does not exist this is where it will be created
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        // create adapter once
-        Cursor c = db.rawQuery("SELECT * FROM " + AddressDatabaseContract.AddressTable.TABLE_NAME, null);
+        Cursor c = db.rawQuery("SELECT * FROM " + AddressDatabaseContract.AddressTable.TABLE_NAME +
+                " ORDER BY " + AddressDatabaseContract.AddressTable.NAME, null);
         Log.d("DEBUG", DatabaseUtils.dumpCursorToString(c));
         int flags = 0; // no auto-requery!
         mAdapter = new SimpleCursorAdapter(context, layout, c, FROM, TO, flags);
-        db.rawQuery(AddressDatabaseContract.AddressTable.DELETE_TABLE, null);
-        Log.d("DEBUG", "Table dropped");
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Intent intent = new Intent(getActivity(), ScrollingAddressEntryActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        // each time we are started use our ListAdapter
+        // each time started use the ListAdapter
         setListAdapter(mAdapter);
-        // and tell loader manager to start loading
-        // THIS LINE REMOVED
     }
-
-    // and name should be displayed in the text1 TextView in item layout
-    private static final String[] FROM = { AddressDatabaseContract.AddressTable.NAME };
-    private static final int[] TO = { android.R.id.text1 };
 }
--- app/src/main/java/com/connor/android_address_book/MainActivity.java
+++ app/src/main/java/com/connor/android_address_book/MainActivity.java
@@ -1,10 +1,7 @@
 package com.connor.android_address_book;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AddressDatabaseHelper dbHelper = new AddressDatabaseHelper(this);
-        // get the database, if it does not exist this is where it will be created
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-
-
     }
 
     protected void onPause(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 }
