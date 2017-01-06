1012da1ee4fab390eeca29f62e67c53b504af04d
Connor Jenkins <cjconnorjenkins@gmail.com>
2017-01-06 02:02:11 -0500

    filling in entry activity xml

--- app/src/main/java/com/connor/android_address_book/ScrollingAddressEntryActivity.java
+++ app/src/main/java/com/connor/android_address_book/ScrollingAddressEntryActivity.java
@@ -1,5 +1,6 @@
 package com.connor.android_address_book;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
@@ -22,32 +23,55 @@ public class ScrollingAddressEntryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling_address_entry);
 
         Intent intent = getIntent();
-        String id = intent.getStringExtra("ID");
+        final String id = intent.getStringExtra("ID");
 
         AddressDatabaseHelper dbHelper = new AddressDatabaseHelper(this);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         // select the row with the _id of the intent we received
-        Cursor c = db.query(AddressDatabaseContract.AddressTable.TABLE_NAME, new String[] {"name", "_id"}, "_id='" + id + "'", null, null, null, null);
+        Cursor c = db.query(AddressDatabaseContract.AddressTable.TABLE_NAME, null, "_id='" + id + "'", null, null, null, null);
         c.moveToFirst();
-        int indexName = c.getColumnIndex(AddressDatabaseContract.AddressTable.NAME);
+        final int Name = c.getColumnIndex(AddressDatabaseContract.AddressTable.NAME);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(c.getString(indexName)); // make the title the current contact's name
+        toolbar.setTitle(c.getString(Name)); // make the title of the activity the current contact's name
         setSupportActionBar(toolbar);
 
+        final Context context = this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view) { // make the edit button start the edit activity
+                Intent intent = new Intent(context, EditAddressActivity.class);
+                intent.putExtra("ID", id);
+                startActivity(intent);
             }
         });
 
+        // getDrawable is deprecated but line doesn't work when I remove it?
         Drawable d = getResources().getDrawable(android.R.drawable.ic_menu_edit); // change icon of fab
         fab.setImageDrawable(d);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        for (int i = 1; i < 11; ++i) {
+            if (c.getString(i) != null) {
+
+            }
+        }
+
     }
 }
+
+//    final int Cell = c.getColumnIndex(AddressDatabaseContract.AddressTable.CELL);
+//        final int Work = c.getColumnIndex(AddressDatabaseContract.AddressTable.WORK);
+//        final int Home = c.getColumnIndex(AddressDatabaseContract.AddressTable.HOME);
+//
+//        final int Personal = c.getColumnIndex(AddressDatabaseContract.AddressTable.PERSONAL);
+//        final int Employee = c.getColumnIndex(AddressDatabaseContract.AddressTable.EMPLOYEE);
+//        final int Misc = c.getColumnIndex(AddressDatabaseContract.AddressTable.MISC);
+//
+//        final int Line1 = c.getColumnIndex(AddressDatabaseContract.AddressTable.LINE1);
+//        final int Line2 = c.getColumnIndex(AddressDatabaseContract.AddressTable.LINE2);
+//        final int CityState = c.getColumnIndex(AddressDatabaseContract.AddressTable.CITYSTATE);
+//        final int ZipCode = c.getColumnIndex(AddressDatabaseContract.AddressTable.ZIPCODE);
