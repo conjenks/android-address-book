007e40c5f2f99736cba3c58bdd0fe7d202b212b0
conjenks <cjconnorjenkins@gmail.com>
2017-01-10 01:53:45 -0500

    having trouble forming the in-code SQL statement to update new database values

--- app/src/main/java/com/connor/android_address_book/EditAddressActivity.java
+++ app/src/main/java/com/connor/android_address_book/EditAddressActivity.java
@@ -29,7 +29,7 @@ public class EditAddressActivity extends AppCompatActivity {
         final String id = intent.getStringExtra("ID");
 
         AddressDatabaseHelper dbHelper = new AddressDatabaseHelper(this);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         // select the row with the _id of the intent we received
         Cursor c = db.query(AddressDatabaseContract.AddressTable.TABLE_NAME, null, "_id='" + id + "'", null, null, null, null);
@@ -39,13 +39,29 @@ public class EditAddressActivity extends AppCompatActivity {
         toolbar.setTitle(c.getString(1)); // make the title of the activity the current contact's name
         setSupportActionBar(toolbar);
 
+        final Resources res = getResources();
+        final String[] columnNames = c.getColumnNames();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+           @Override
+           public void onClick(View view) { // save the EditText changes to the database
+               String statement = "UPDATE " + AddressDatabaseContract.AddressTable.TABLE_NAME + " SET "; // start constructing the SQL statement
+               for (int i = 1; i < 12; ++i) {
+                   int viewId = res.getIdentifier("editText" + Integer.toString(i), "id", "com.connor.android_address_book"); // get the id of an EditText by its integer title
+                   EditText edit = (EditText) findViewById(viewId);
+                   String text = edit.getText().toString();
+                   if (text.trim().length() != 0) { // if the EditText field is NOT blank
+                       statement += columnNames[i] + "='" + text + "'";
+                       Log.d("DEBUG", Integer.toString(i));
 
-            }
-        });
+                   }
+               }
+
+               statement += "WHERE _id='" + id + "';";
+               db.execSQL(statement);
+           }
+       });
 
         // getDrawable is deprecated but line doesn't work when I remove it?
         Drawable d = getResources().getDrawable(android.R.drawable.ic_menu_save); // change icon of fab
@@ -53,8 +69,7 @@ public class EditAddressActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Resources res = getResources();
-        for (int i = 1; i < 12; ++i) { // this loop access each db column's
+        for (int i = 1; i < 12; ++i) { // this loop accesses each db column's
             int viewId = res.getIdentifier("editText" + Integer.toString(i), "id", this.getPackageName()); // get the id of an EditText by its integer title
             if (c.getString(i) != null) { // if this column has no value, hide it
                 EditText editText = (EditText) findViewById(viewId);
